44d2b4fc55db8d6d49b2d3cdb160b311c536b658
Akintola Marke <amarke@LT-C02GF4K5MD6R.local>
2022-01-16 14:56:00 -0500

    small changes

---
+++ js/mixpanel.js
@@ -0,0 +1,5 @@
+tutorial_button = document.querySelector("a.btn.btn-outline-white.btn-lg.waves-effect.waves-light")
+
+function test(){
+    tutorial_button.addEventListener("click", function(){ console.log("Hello World!"); });
+}
